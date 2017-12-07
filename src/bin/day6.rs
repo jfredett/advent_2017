4e44606fef42999a16f4903f9557ebeca76c9d79
Joe Fredette <jfredett@gmail.com>
2017-12-07 04:09:38 +0000

    [Advent Day 6] part 1 complete

---
+++ src/bin/day6.rs
@@ -0,0 +1,139 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashSet;
+
+fn main() {
+  println!("Advent of Code Day 6");
+  println!("");
+  println!("http://adventofcode.com/2017/day/5");
+  println!("");
+
+  let mut file = File::open("data/day6/input").expect("file not found");
+  let mut content = String::new();
+  file.read_to_string(&mut content).expect("Something went wrong reading input file");
+
+  println!("Part 1: {}", part1(&content));
+  //println!("Part 2: {}", part2(&content));
+}
+
+fn part1(input: &String) -> i32 {
+  return MemoryBank::new(input, Mode::Part1).run();
+}
+
+
+#[derive(Debug, PartialEq, Eq)]
+enum Mode {
+  Part1,
+  Part2
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct MemoryBank {
+  source: String,
+  history: HashSet<Vec<i32>>,
+  blocks: Vec<i32>,
+  mode: Mode,
+  steps: i32,
+  loop_found: bool
+}
+
+impl MemoryBank {
+  pub fn new(source: &String, mode: Mode) -> MemoryBank {
+    return MemoryBank {
+      source: source.to_owned(),
+      history: HashSet::new(),
+      blocks: vec![],
+      mode: mode,
+      steps: 0,
+      loop_found: false
+    };
+  }
+
+  pub fn run(&mut self) -> i32 {
+    self.reboot();
+
+    while !self.loop_found {
+      self.step();
+      self.steps += 1;
+      self.record_history();
+    }
+
+    return self.steps
+  }
+
+  fn step(&mut self) {
+    let start_idx = self.largest_block_id();
+    let memory = self.blocks[start_idx];
+    let len = self.blocks.len();
+
+    self.blocks[start_idx] = 0;
+    for offset in 0..memory {
+      self.blocks[(start_idx + (offset as usize) + 1) % len] += 1;
+    }
+  }
+
+  fn seen(&self, entry: &Vec<i32>) -> bool {
+    return self.history.get(entry).is_some();
+  }
+
+  fn record_history(&mut self) {
+    let entry = self.blocks.to_owned();
+    self.loop_found = self.seen(&entry);
+    self.history.insert(entry.to_owned());
+  }
+
+  fn largest_block_id(&self) -> usize {
+    let max = self.largest_block_allocation();
+
+    for idx in 0..self.blocks.len() {
+      if self.blocks[idx] == max {
+        return idx;
+      }
+    }
+    panic!("Cannot occur!");
+  }
+
+  fn largest_block_allocation(&self) -> i32 {
+    match self.blocks.iter().max() {
+      Some(v) => return v.to_owned(),
+      None => panic!("No max value found!")
+    }
+  }
+
+  fn reboot(&mut self) {
+    self.blocks = vec![];
+    self.history.clear();
+    self.steps = 0;
+    self.loop_found = false;
+    self.parse_source();
+  }
+
+  fn parse_source(&mut self) {
+    for item in self.source.split_whitespace() {
+      let parsed : Result<i32, std::num::ParseIntError> = item.parse();
+      match parsed {
+        Ok(d) => self.blocks.push(d),
+        Err(e) => panic!("Memory failed to parse {} as a number with error: {}", item, e)
+      }
+    }
+  }
+
+}
+
+#[test]
+fn part1_test() {
+  let mut file = File::open("data/day6/test").expect("file not found");
+  let mut content = String::new();
+  file.read_to_string(&mut content).expect("Something went wrong reading test file");
+
+  assert_eq!(MemoryBank::new(&content, Mode::Part1).run(), 5);
+}
+
+//#[test]
+//fn part2_test() {
+  //let mut file = File::open("data/day5/test").expect("file not found");
+  //let mut content = String::new();
+  //file.read_to_string(&mut content).expect("Something went wrong reading test file");
+
+  //assert_eq!(Machine::new(content, Mode::Part2, false).run(), 10);
+//}
